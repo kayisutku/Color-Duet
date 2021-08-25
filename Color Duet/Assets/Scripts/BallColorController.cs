402507f7b2228fe9046444e2c86c98c3a53552dc
Mehmet Yazıcı <32649344+Mehmet-Yazici@users.noreply.github.com>
2021-08-25 14:28:01 +0300

    added color changer

---
+++ Color Duet/Assets/Scripts/BallColorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallColorController : MonoBehaviour
+{
+    [SerializeField] float colorChangeHeight = 0.8f;
+    Color ownColor;
+    Color lerpColor;
+    [SerializeField] Color mixColor;
+
+    Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        //renderer.material.SetColor("_Color", mixColor);
+        ownColor = renderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.y)  < colorChangeHeight)
+        {
+            renderer.material.SetColor("_Color", Color.Lerp(ownColor , mixColor, (colorChangeHeight - Mathf.Abs(transform.position.y) / colorChangeHeight    )));
+        } 
+    }
+
+    
+}
