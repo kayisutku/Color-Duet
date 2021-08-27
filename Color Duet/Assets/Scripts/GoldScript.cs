40965b250ec1c0240c0fbcfb184f824a97f5d4f6
Mehmet Yazıcı <32649344+Mehmet-Yazici@users.noreply.github.com>
2021-08-27 13:37:19 +0300

    Gold particle added

--- Color Duet/Assets/Scripts/GoldScript.cs
+++ Color Duet/Assets/Scripts/GoldScript.cs
@@ -9,6 +9,7 @@ public class GoldScript : MonoBehaviour
     [SerializeField] private float swayAmount = 2f;
     [SerializeField] private float timeDuration = 0.75f;
     [SerializeField] private float rotateAmount = 5f;
+    public ParticleSystem goldParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +26,14 @@ public class GoldScript : MonoBehaviour
     void Update()
     {
         transform.Rotate(0, rotateAmount * Time.deltaTime,0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<BallController>())
+        {
+            Instantiate(goldParticle, transform.position, goldParticle.transform.rotation);
+            Destroy(gameObject);
+        }
     }
 }
