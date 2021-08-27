65e94e6aa1fc113026a2e0315442b03e719e3017
Mehmet Yazıcı <32649344+Mehmet-Yazici@users.noreply.github.com>
2021-08-27 15:13:57 +0300

    finish added

---
+++ Color Duet/Assets/FinishScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishScript : MonoBehaviour
+{
+    public GameObject horizontalBallMovement1;
+    public GameObject horizontalBallMovement2;
+    public GameObject horizontalBallMovement3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<BallController>())
+        {
+            horizontalBallMovement1.GetComponent<HorizontalBallMovement>().ballSpeed = 0;
+            horizontalBallMovement2.GetComponent<HorizontalBallMovement>().ballSpeed = 0;
+            horizontalBallMovement3.GetComponent<HorizontalBallMovement>().ballSpeed = 0;
+        }
+    }
+}
