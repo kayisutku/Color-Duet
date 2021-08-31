14640cba0d019a63faab137e5ac1b596825a0ca2
kayisutku <kayisutku@gmail.com>
2021-08-31 09:31:34 +0300

    @ finish line

---
+++ Color Duet/Assets/Scripts/Finish.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    public GameObject horizontalBallMovement1;
+    public GameObject horizontalBallMovement2;
+    public GameObject horizontalBallMovement3;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<BallController>())
+        {
+            horizontalBallMovement1.GetComponent<HorizontalBallMovement>().ballSpeed = 0;
+            horizontalBallMovement2.GetComponent<HorizontalBallMovement>().ballSpeed = 0;
+            horizontalBallMovement3.GetComponent<HorizontalBallMovement>().ballSpeed = 0;
+        }
+    }
+}
